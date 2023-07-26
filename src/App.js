a4045f21ad7228d59cb7de6251208fd03081ec2e
Chandan Janardan <chandanjanardan@gmail.com>
2023-07-26 22:12:57 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import MyComponent from './myComponent';
+import { useEffect } from 'react';
 
 function App() {
+  const loadScript = () => {
+    const script = document.createElement("script");
+    script.src = "https://otpless.com/auth.js";
+    script.id = "otplessIdScript"
+    document.body.appendChild(script);
+  }
+useEffect(() => {
+  
+  loadScript()
+  window.otpless = (otplessUser) => {
+     
+    console.log(otplessUser)
+  };
+  return () => {
+    let scr = document.getElementById('otplessIdScript')
+    document.body.removeChild(scr);
+  };
+}, []);
   return (
-    <div className="App">
-      <MyComponent />
+    <div id ="otpless" className="App" custom="true">
+      <button>hehe</button>
     </div>
   );
 }
